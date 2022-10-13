0a98489c0481bcc701904b982ebc4d1f10171555
takashi247 <takashi247@gmail.com>
2022-10-14 01:35:24 +0900

    complete until lesson 11

---
+++ socket_io_101/basics.js
@@ -0,0 +1,22 @@
+const http = require("http");
+const socketio = require("socket.io");
+
+const server = http.createServer((req, res) => {
+  res.end("I am connected!");
+});
+
+const io = socketio(server, {
+  cors: {
+    origin: "null",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket, req) => {
+  socket.emit("welcome", "Welcome to the websocket server!!");
+  socket.on("message", (msg) => {
+    console.log(msg);
+  });
+});
+
+server.listen(8000);
