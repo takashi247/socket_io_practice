d739e149e59b53eeab73030f335d657ef5ffb196
takashi247 <takashi247@gmail.com>
2022-10-14 20:13:54 +0900

    copy 101 to 201

---
+++ socket_io_201/chat.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const socketio = require("socket.io");
+
+app.use(express.static(__dirname + "/public"));
+
+const expressServer = app.listen(9000);
+const io = socketio(expressServer);
+io.on("connection", (socket) => {
+  socket.emit("messageFromServer", { data: "Welcome to socket.io server" });
+  socket.on("messageToServer", (dataFromClient) => {
+    console.log(dataFromClient);
+  });
+  socket.on("newMessageToServer", (msg) => {
+    io.emit("messageToClients", { text: msg.text });
+  });
+});
